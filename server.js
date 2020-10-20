b443fded4b104564ea16d9abbcbf4d9a769ca1e9
iromashko <iromashko@me.com>
2020-10-20 11:56:02 +0300

    add index page

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const path = require('path');
+
+app.use(helmet());
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send({ hello: 'world' });
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.get('/url-shortener', (req, res) => {
